911e0f28dbc45cb849f06c4a23804e361a1a0b21
Mr. Slippery <badc0ded@gmail.com>
2026-02-13 20:52:31 +0200

    Bring local and ci build together

--- check.sh
+++ check.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+FMT_ARGS=""
+if [ "${1:-}" = "--check" ]; then
+    FMT_ARGS="--check"
+fi
+
 cargo clippy --workspace --exclude scmm-ebpf -- -D warnings
-cargo fmt --package scmm-common --package scmm-record --package scmm-extract --package scmm-compile --package scmm-enforce --package xtask
+cargo fmt $FMT_ARGS --package scmm-common --package scmm-record --package scmm-extract --package scmm-compile --package scmm-enforce --package xtask
